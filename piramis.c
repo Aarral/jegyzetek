65647c07cb12004dc0050712545bc8313c6405b8
Aarral <fadebullets57@gmail.com>
2023-09-25 09:37:44 +0200

    -eloadas4 git status git commit -m  -eloadas4

--- piramis.c
+++ piramis.c
@@ -3,21 +3,7 @@
 
 int main(int argc, char **argv)
 {
-	int n=15;
-	for (int i = 0; i < n; i++)
-	{	
-		
-		for (int j = 0; j < n-1-i; j++)
-		{
-				printf(" ");
-		}
-		for (int j = 0; j < (i*2)+1; j++)
-		{
-				printf("X");
-		}
-		printf("\n");
-	}
-	
+	printf("\\");
 	return 0;
 }
 
