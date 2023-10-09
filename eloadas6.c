5ab9cc416f2d1a9110c846e572e24f6cfecc7260
Aarral <fadebullets57@gmail.com>
2023-10-09 16:04:00 +0200

    szorgalmi

---
+++ eloadas6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int x =0;
+	double y=2.46;
+	x=y;
+	printf("%d", x);
+	return 0;
+}
+
--- piramis.c
+++ piramis.c
@@ -3,7 +3,16 @@
 
 int main(int argc, char **argv)
 {
-	printf("\\");
+	int n=1000;
+	int tomb[1000];
+	for(int i=0;i<n;++i)
+		tomb[i]=i;	
+	for(int i=2;i<n;++i)
+		for (int j=2;j<n;j++)
+			if(tomb[i]%j==0&&i!=j){tomb[i]=-1;break;}
+	for(int i=2;i<n;++i)
+		if(tomb[i]!=-1)
+		printf("%d ",tomb[i]);
 	return 0;
 }
 
